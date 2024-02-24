1de8e5bcfa1bb447c9c268b76545de5976c12d3d
beingrajput7233 <rajputashu7200@gmail.com>
2024-02-24 15:30:57 +0530

    JWT-2(Jwt backend running)

---
+++ src/main/java/com/exam/examserver/entity/JwtResponse.java
@@ -0,0 +1,68 @@
+package com.exam.examserver.entity;
+
+public class JwtResponse {
+
+    private String jwtToken;
+
+    private String username;
+
+    public JwtResponse() {
+    }
+
+    public JwtResponse(String jwtToken, String username) {
+        this.jwtToken = jwtToken;
+        this.username = username;
+    }
+
+    public String getJwtToken() {
+        return jwtToken;
+    }
+
+    public void setJwtToken(String jwtToken) {
+        this.jwtToken = jwtToken;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public String toString() {
+        return "JwtResponse{" +
+                "jwtToken='" + jwtToken + '\'' +
+                ", username='" + username + '\'' +
+                '}';
+    }
+
+    public static class Builder {
+        private String jwtToken;
+        private String username;
+
+        public Builder jwtToken(String jwtToken) {
+            this.jwtToken = jwtToken;
+            return this;
+        }
+
+        public Builder username(String username) {
+            this.username = username;
+            return this;
+        }
+
+        public JwtResponse build() {
+            JwtResponse jwtResponse = new JwtResponse();
+            jwtResponse.jwtToken = this.jwtToken;
+            jwtResponse.username = this.username;
+            return jwtResponse;
+        }
+    }
+
+    // Builder method
+    public static Builder builder() {
+        return new Builder();
+    }
+
+}
