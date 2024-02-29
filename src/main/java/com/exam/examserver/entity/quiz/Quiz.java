1f94e9ca743906619aa6bb1c0b0bf3a69acbe063
beingrajput7233 <rajputashu7200@gmail.com>
2024-02-29 14:39:20 +0530

    Entities added for category,quiz and questions

---
+++ src/main/java/com/exam/examserver/entity/quiz/Quiz.java
@@ -0,0 +1,94 @@
+package com.exam.examserver.entity.quiz;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class Quiz {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long qId;
+
+    private String title;
+
+    private String description;
+
+    private Boolean active=false;
+
+    private String numberOfQuestions;
+
+    private String maxMarks;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    private Category category;
+
+
+    @OneToMany(mappedBy = "quiz",fetch = FetchType.LAZY,cascade = CascadeType.ALL)
+    @JsonIgnore
+    private Set<Question> questions=new HashSet<>();
+
+
+
+
+    public Quiz(){}
+
+    public Long getqId() {
+        return qId;
+    }
+
+    public void setqId(Long qId) {
+        this.qId = qId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    public String getNumberOfQuestions() {
+        return numberOfQuestions;
+    }
+
+    public void setNumberOfQuestions(String numberOfQuestions) {
+        this.numberOfQuestions = numberOfQuestions;
+    }
+
+    public String getMaxMarks() {
+        return maxMarks;
+    }
+
+    public void setMaxMarks(String maxMarks) {
+        this.maxMarks = maxMarks;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+}
